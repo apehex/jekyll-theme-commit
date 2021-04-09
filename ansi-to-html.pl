5f99b989cc6c6bfb6bc1767caecc73dbc4eee436
apehex <apehex@protonmail.com>
2021-04-09 15:27:53 +0200

    Add a step to translate ansi codes to meaningful css classes

--- ansi-to-html.pl
+++ ansi-to-html.pl
@@ -3,6 +3,8 @@
 while (<>) {
     @F = split /\e\[m/;
     print "<div class=\"line\">" . join("</span>",
+        # translate the ANSI code to a HTML class
+        map {s/class="([0-9;]+)"/class="fg-bg-"/r} # TODO: css syntax + role
         # replace ANSI color codes with a span
         map {s/ ?\e\[([0-9;]+m)/<span class="$1">/r}
         # add missing opening tags when there's no color code
