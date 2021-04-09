39194becb265c214de236ac1ab7f762b9f8f0501
apehex <apehex@protonmail.com>
2021-04-09 15:32:27 +0200

    List various format possibilities

---
+++ commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# options:
+# - plain vs escaped (html)
+# - header or not
+# - hash or not
+# -
+
+git log --graph --full-history --all \
+  --pretty=format:"%h%x09%d%x20%s%n" \
+  --patch --color=always --expand-tabs=4 --word-diff=color
+
+perl -ne 'm#(\e\[([0-9;]+m).+?(?=\e\[m))#gm && print "#$1#$2\n"'
+
+git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'
+git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
+git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'
